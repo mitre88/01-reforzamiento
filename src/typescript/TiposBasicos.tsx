7a1fec902faa16f92cad9a91e24e1608675a02cf
mitre88 <bedr10_capacitacion@hotmail.com>
2022-08-22 21:21:37 -0500

    arrays en ts y react

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,13 +1,22 @@
-
-
 export const TiposBasicos = () => {
+  const name: string = "Alejandro";
+  const edad: number = 40;
+  const active: boolean = false;
+
+  const hobbies: string[] = ["futbol", "natacion", "caminar"];
+
   return (
     <>
       <h3>
         Tipos Basicos
+        {name}-{edad}-{ active? "activo" : "inactivo" }
       </h3>
+      
+     { hobbies.map((hobby, index) => (
+        <p key={index}>{hobby}</p>
+      ))}
+       
+
     </>
-  )
-} ;
-
-
+  );
+};
